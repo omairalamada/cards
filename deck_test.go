37c9e8bd06dac2d625a25fe6c84be6d2171895e7
omairalamada <omairmangondaya@gmail.com>
2022-07-20 16:52:40 +0800

    added additional test scripts

--- deck_test.go
+++ deck_test.go
@@ -2,10 +2,18 @@ package main
 
 import "testing"
 
-func TestNewDeck(t *testing.T) { // t is test handler
+func TestNewDeck(t *testing.T) { // t a is test handler
 	d := newDeck()
 
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
 }
