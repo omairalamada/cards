5d41772f9bc2b3235482fdd4ff38aecf522f8466
omairalamada <omairmangondaya@gmail.com>
2022-07-20 10:06:33 +0800

    Reading From the Hard Drive

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -42,4 +43,17 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		// option #1 - log the error and return a call to newDeck()
+		// code here
+
+		// option #2 - log the error and entirely quit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
 }
