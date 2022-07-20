58e9133726995fdb4dc2559f58048b3d4f8c4434
omairalamada <omairmangondaya@gmail.com>
2022-07-20 11:03:44 +0800

    refactor, added Random Number Generation.

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of 'deck'
@@ -62,10 +63,12 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano()) // seed // using to generate a different INT 64 nuber.
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
-
 	}
 }
